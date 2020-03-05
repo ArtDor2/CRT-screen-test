81bd46c50eeffae4bb043bbfbdb99acdbbb8ef50
Artur Dorovskikh <19955193+ArtDor2@users.noreply.github.com>
2020-03-05 00:20:43 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Made by Artur Dorovskikh
-# CRT-screen-test v0.2 2020-03-02
+# CRT-screen-test v0.3 2020-03-05
 # This app is primarily for calibrating CRT computer monitors
 
 # TODO make spacing with commas neater
